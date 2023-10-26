d9c3ba28febcfd78134ea7848afe2491e2ac36c8
Laurent Vrevin <laurent.vrevin@hotmail.fr>
2023-10-26 15:04:31 +0200

    Clean code to develop from scratch

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
@@ -5,25 +5,10 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private var textViewMain: TextView? = null
-    private var textViewQuantity: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textViewMain = findViewById(R.id.activity_main_activity_text_view_main)
-        textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity)
-        configureTextViewMain()
-        configureTextViewQuantity()
-    }
 
-    private fun configureTextViewMain() {
-        textViewMain!!.textSize = 15f
-        textViewMain!!.text = "Le premier bien immobilier enregistré vaut "
-    }
-
-    private fun configureTextViewQuantity() {
-        val quantity = Utils.convertDollarToEuro(100)
-        textViewQuantity!!.textSize = 20f
-        textViewQuantity!!.text = quantity.toString()
     }
 }
